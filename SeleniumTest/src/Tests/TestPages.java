c6282973e1e3d3152268b902910912d4a0833d7d
Blagovest Simov <77917533+Blago-Simov@users.noreply.github.com>
2023-12-04 20:01:13 +0200

    Update TestPages.java

--- SeleniumTest/src/Tests/TestPages.java
+++ SeleniumTest/src/Tests/TestPages.java
@@ -41,53 +41,52 @@ public class TestPages {
     }
 
     @Test(priority = 2)
-    public void successfulLogin() {
+    public void successfulLogin()  {
         homePage.fillTheLoginForm(driver);
         Assert.assertTrue(elementsPage.isLogOutButtonDisplayed(driver));
+        elementsPage.clickOnLogout(driver);
+
     }
 
     @Test(priority = 3)
-    public void successfulLogOut() {
+    public void successfulLogOut()  {
         homePage.fillTheLoginForm(driver);
         Assert.assertTrue(elementsPage.isLogOutButtonDisplayed(driver));
         elementsPage.clickOnLogout(driver);
         Assert.assertTrue(homePage.getHomePageHeaderText(driver).contains("Hey! Wait a minute... What is this site?"));
+
     }
 
     @Test(priority = 4)
-    public void testSalesFillingForm() {
+    public void testSalesFillingForm()  {
         homePage.fillTheLoginForm(driver);
-        elementsPage.fillSalesForm(driver, "Angel", "Angelov", "5000", "7000");
+        elementsPage.fillSalesForm(driver, "Angel", "Angelov", "5000", 7000);
         elementsPage.clickOnSubmit(driver);
-        String expectedName = "Angel Angelov";
-        Assert.assertEquals("[ERROR] Different than the expected Name!"
-                , expectedName, elementsPage.getTableSummaryName(driver));
-        Assert.assertTrue(elementsPage.getActiveSalesPeopleCount() > 0);
-        Assert.assertEquals((elementsPage.getActivePeopleSalesResult(driver))
-                , "Active sales people:" + "\n" + elementsPage.getActiveSalesPeopleCount().toString());
+        Assert.assertTrue("[ERROR] Different than the expected Name!"
+                , elementsPage.getSummaryTableElement(driver,"Angel"));
+        Assert.assertTrue(elementsPage.getSalesPeopleCount() > 0);
+        Assert.assertEquals(elementsPage.getActiveSalesPeopleElement(driver),"Active sales people:" +"\n"+
+               elementsPage.getSalesPeopleCount().toString());
 
     }
 
     @Test(priority = 5)
-    public void testCalculationSalesForm() {
+    public void testCalculationSalesForm() throws InterruptedException {
         homePage.fillTheLoginForm(driver);
-        elementsPage.fillSalesForm(driver, "Petar", "Petrov", "5000", "8000");
+        elementsPage.fillSalesForm(driver, "Petar", "Petrov", "75000", 8008);
         elementsPage.clickOnSubmit(driver);
-        Assert.assertEquals((elementsPage.getActivePeopleSalesResult(driver))
-                , "Active sales people:" + "\n" + elementsPage.getActiveSalesPeopleCount().toString());
-        Assert.assertEquals((elementsPage.getExpectedSalesResult(driver))
-                , "Expected sales:" + "\n" + "$5,000");
-        Assert.assertEquals((elementsPage.getActualSalesResult(driver))
-                , "Actual sales:" + "\n" + "$8,000");
-        Assert.assertEquals((elementsPage.getDifferenceResult(driver))
-                , "Difference:" + "\n" + "$3,000");
+        Assert.assertEquals(elementsPage.getActiveSalesPeopleElement(driver),
+                "Active sales people:"+"\n"+elementsPage.getSalesPeopleCount().toString());
+        Assert.assertTrue((elementsPage.getCalculationTableElement(driver,"75000")));
+        Assert.assertTrue((elementsPage.getCalculationTableElement(driver,"8008")));
+        Assert.assertTrue((elementsPage.getCalculationTableElement(driver,"$-66,992")));
 
     }
 
     @Test(priority = 6)
-    public void testPerformanceButtonFunctionality() {
+    public void testPerformanceButtonFunctionality() throws InterruptedException {
         homePage.fillTheLoginForm(driver);
-        elementsPage.fillSalesForm(driver, "Anton", "Angelov", "10000", "15000");
+        elementsPage.fillSalesForm(driver, "Anton", "Angelov", "10000", 15000);
         elementsPage.clickOnSubmit(driver);
         Assert.assertTrue(elementsPage.isPerformanceButtonDisplayed(driver));
         elementsPage.clickOnPerformanceButton(driver);
@@ -95,23 +94,49 @@ public class TestPages {
         Assert.assertEquals(expectedMessage, elementsPage.getPerformanceMessage(driver));
 
     }
-    }
 
     @Test(priority = 7)
-    public void testSummaryTable(){
+    public void testDeleteButtonFunctionality(){
         homePage.fillTheLoginForm(driver);
-        elementsPage.fillSalesForm(driver, "Plamen", "Petrov", "5000", "8000");
+        elementsPage.fillSalesForm(driver, "Victor", "Ivanov", "10000", 15000);
         elementsPage.clickOnSubmit(driver);
-        String expectedName = "Plamen Petrov";
-        Assert.assertEquals("[ERROR] Different than the expected Name!"
-                , expectedName, elementsPage.getTableSummaryName(driver));
-        Assert.assertEquals("[ERROR] Different than the expected Target!"
-                , "$5,000", elementsPage.getTableSummaryTarget(driver));
-        Assert.assertEquals("[ERROR] Different than the expected Result!"
-                , "$8,000", elementsPage.getTableSummaryResult(driver));
-        Assert.assertEquals("$3,000", elementsPage.getTableSummaryDifference(driver));
+        Assert.assertTrue(elementsPage.isPerformanceButtonDisplayed(driver));
+        elementsPage.clickOnDeleteButton(driver);
+        Assert.assertTrue(elementsPage.isPerformanceTableEmpty());
 
     }
+
+    @Test(priority = 8)
+    public void testSummaryTable(){
+        homePage.fillTheLoginForm(driver);
+        elementsPage.fillSalesForm(driver, "Plamen", "Petrov", "5000", 8000);
+        elementsPage.clickOnSubmit(driver);
+        elementsPage.fillSalesForm(driver, "Victor", "Markov", "75000", 4003);
+        elementsPage.clickOnSubmit(driver);
+        elementsPage.fillSalesForm(driver, "Svilen", "Svilenov", "25000", 36009);
+        elementsPage.clickOnSubmit(driver);
+        Assert.assertTrue("[ERROR] Different than the expected Name!",
+                elementsPage.getSummaryTableElement(driver,"Plamen Petrov"));
+        Assert.assertTrue("[ERROR] Different than the expected Name!",
+                elementsPage.getSummaryTableElement(driver,"Victor Markov"));
+        Assert.assertTrue("[ERROR] Different than the expected Name!",
+                elementsPage.getSummaryTableElement(driver,"Svilen Svilenov"));
+        Assert.assertTrue("[ERROR] Different than the expected Target!",
+                elementsPage.getSummaryTableElement(driver,"$5,000"));
+        Assert.assertTrue("[ERROR] Different than the expected Target!",
+                elementsPage.getSummaryTableElement(driver,"$75,000"));
+        Assert.assertTrue("[ERROR] Different than the expected Target!",
+                elementsPage.getSummaryTableElement(driver,"$25,000"));
+        Assert.assertTrue("[ERROR] Different than the expected Result!"
+                , elementsPage.getSummaryTableElement(driver,"$8,000"));
+        Assert.assertTrue("[ERROR] Different than the expected Result!"
+                , elementsPage.getSummaryTableElement(driver,"$4,003"));
+        Assert.assertTrue("[ERROR] Different than the expected Result!"
+                , elementsPage.getSummaryTableElement(driver,"$36,009"));
+        elementsPage.clickOnDeleteButton(driver);
+
+    }
+
     @AfterTest
     public void quitDriver() {
         driver.quit();
