09b0bf7aae0912372e6d68f22b1c507b657d1d6a
Blagovest Simov <77917533+Blago-Simov@users.noreply.github.com>
2024-01-09 19:27:13 +0200

    Update TestPages.java

--- SeleniumTest/src/Tests/TestPages.java
+++ SeleniumTest/src/Tests/TestPages.java
@@ -17,106 +17,48 @@ public class TestPages {
 
     ElementsPage elementsPage;
 
+    final String userName = "maria";
+    final String password = "thoushallnotpass";
 
     @BeforeTest
-    public void loadHomePage() throws InterruptedException {
+    public void loadHomePage() {
         homePage = new HomePage();
         elementsPage = new ElementsPage();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        
+
     }
 
-    @Test(priority = 0)
-    public void testTheHomePageUrl() throws InterruptedException {
+    @Test(priority = 1)
+    public void testTheHome()  {
         driver.get(homePage.getHomePageUrl());
         String currentUrl = driver.getCurrentUrl();
         String expectedUrl = homePage.getHomePageUrl() + "/";
         Assert.assertEquals("[ERROR] Different than the expected URL!", expectedUrl, currentUrl);
-       
+        Assert.assertTrue(homePage.getHomePageHeaderText(driver).contains("Hey! Wait a minute... What is this site?"));
     }
 
-    @Test(priority = 1)
-    public void checkHomePageHeaderText() throws InterruptedException {
-        driver.get(homePage.getHomePageUrl());
-        Assert.assertTrue(homePage.getHomePageHeaderText(driver).contains("Hey! Wait a minute... What is this site?"));
-        
-    }
 
     @Test(priority = 2)
-    public void successfulLogin() throws InterruptedException {
+    public void successfulLogin() {
         driver.get(homePage.getHomePageUrl());
-        homePage.fillTheLoginForm(driver);
-        Assert.assertTrue(elementsPage.isLogOutButtonDisplayed(driver));
-        elementsPage.clickOnLogout(driver);
-        Thread.sleep(2000);
+        homePage.fillTheLoginForm(driver,userName,password);
+        Assert.assertTrue(elementsPage.isUsernameDisplayed(driver));
 
     }
-
     @Test(priority = 3)
-    public void successfulLogOut() throws InterruptedException {
+    public void successfulLogout(){
         driver.get(homePage.getHomePageUrl());
-        homePage.fillTheLoginForm(driver);
-        Assert.assertTrue(elementsPage.isLogOutButtonDisplayed(driver));
+        homePage.fillTheLoginForm(driver,userName,password);
+        Assert.assertTrue(elementsPage.isUsernameDisplayed(driver));
         elementsPage.clickOnLogout(driver);
         Assert.assertTrue(homePage.getHomePageHeaderText(driver).contains("Hey! Wait a minute... What is this site?"));
-       
     }
 
     @Test(priority = 4)
-    public void testSalesFillingForm() throws InterruptedException {
+    public void testDeleteButtonFunctionality() {
         driver.get(homePage.getHomePageUrl());
-        homePage.fillTheLoginForm(driver);
-        elementsPage.fillSalesForm(driver, "Angel", "Angelov", "5000", 7000);
-        elementsPage.clickOnSubmit(driver);
-        Assert.assertTrue("[ERROR] Different than the expected Name!"
-                , elementsPage.getSummaryTableElement(driver,"Angel"));
-        Assert.assertTrue(elementsPage.getSalesPeopleCount() > 0);
-        Assert.assertEquals(elementsPage.getActiveSalesPeopleElement(driver),"Active sales people:" +"\n"+
-               elementsPage.getSalesPeopleCount().toString());
-        elementsPage.clickOnDeleteButton(driver);
-        elementsPage.clickOnLogout(driver);
-        Assert.assertTrue(homePage.getHomePageHeaderText(driver).contains("Hey! Wait a minute... What is this site?"));
-        
-
-    }
-
-    @Test(priority = 5)
-    public void testCalculationSalesForm() throws InterruptedException {
-        driver.get(homePage.getHomePageUrl());
-        homePage.fillTheLoginForm(driver);
-        elementsPage.fillSalesForm(driver, "Petar", "Petrov", "75000", 8008);
-        elementsPage.clickOnSubmit(driver);
-        Assert.assertEquals(elementsPage.getActiveSalesPeopleElement(driver),
-                "Active sales people:"+"\n"+elementsPage.getSalesPeopleCount().toString());
-        Assert.assertTrue((elementsPage.getCalculationTableElement(driver,"75000")));
-        Assert.assertTrue((elementsPage.getCalculationTableElement(driver,"8008")));
-        Assert.assertTrue((elementsPage.getCalculationTableElement(driver,"$-66,992")));
-        elementsPage.clickOnDeleteButton(driver);
-        elementsPage.clickOnLogout(driver);
-        Assert.assertTrue(homePage.getHomePageHeaderText(driver).contains("Hey! Wait a minute... What is this site?"));
-       
-    }
-
-    @Test(priority = 6)
-    public void testPerformanceButtonFunctionality() throws InterruptedException {
-        driver.get(homePage.getHomePageUrl());
-        homePage.fillTheLoginForm(driver);
-        elementsPage.fillSalesForm(driver, "Anton", "Angelov", "10000", 15000);
-        elementsPage.clickOnSubmit(driver);
-        Assert.assertTrue(elementsPage.isPerformanceButtonDisplayed(driver));
-        elementsPage.clickOnPerformanceButton(driver);
-        Assert.assertEquals("A positive result. Well done!", elementsPage.getPerformanceMessage(driver));
-        elementsPage.clickOnDeleteButton(driver);
-        elementsPage.clickOnLogout(driver);
-        Assert.assertTrue(homePage.getHomePageHeaderText(driver).contains("Hey! Wait a minute... What is this site?"));
-        
-    }
-
-    @Test(priority = 7)
-    public void testDeleteButtonFunctionality() throws InterruptedException {
-        driver.get(homePage.getHomePageUrl());
-        homePage.fillTheLoginForm(driver);
+        homePage.fillTheLoginForm(driver,userName,password);
         elementsPage.fillSalesForm(driver, "Victor", "Ivanov", "10000", 15000);
         elementsPage.clickOnSubmit(driver);
         Assert.assertTrue(elementsPage.isPerformanceButtonDisplayed(driver));
@@ -127,10 +69,62 @@ public class TestPages {
 
     }
 
-    @Test(priority = 8)
-    public void testSummaryTable() throws InterruptedException {
+    @Test(priority = 5)
+    public void testSalesFillingForm()  {
         driver.get(homePage.getHomePageUrl());
-        homePage.fillTheLoginForm(driver);
+        homePage.fillTheLoginForm(driver,userName,password);
+        elementsPage.fillSalesForm(driver, "Angel", "Angelov", "5000", 7000);
+        elementsPage.clickOnSubmit(driver);
+        Assert.assertTrue("[ERROR] Different than the expected Name!"
+                , elementsPage.getSummaryTableElement(driver,"Angel"));
+        Assert.assertTrue(elementsPage.getSalesPeopleCount() > 0);
+        Assert.assertEquals(elementsPage.getActiveSalesPeopleElement(driver),"Active sales people:" +"\n"+
+               elementsPage.getSalesPeopleCount().toString());
+        elementsPage.clickOnDeleteButton(driver);
+        elementsPage.clickOnLogout(driver);
+        Assert.assertTrue(homePage.getHomePageHeaderText(driver).contains("Hey! Wait a minute... What is this site?"));
+
+
+    }
+
+    @Test(priority = 6)
+    public void testCalculationSalesForm()  {
+        driver.get(homePage.getHomePageUrl());
+        homePage.fillTheLoginForm(driver,userName,password);
+        elementsPage.fillSalesForm(driver, "Petar", "Petrov", "75000", 8008);
+        elementsPage.clickOnSubmit(driver);
+        Assert.assertEquals(elementsPage.getActiveSalesPeopleElement(driver),
+                "Active sales people:"+"\n"+elementsPage.getSalesPeopleCount().toString());
+        Assert.assertTrue((elementsPage.getCalculationTableElement(driver,"75000")));
+        Assert.assertTrue((elementsPage.getCalculationTableElement(driver,"8008")));
+        Assert.assertTrue((elementsPage.getCalculationTableElement(driver,"$-66,992")));
+        elementsPage.clickOnDeleteButton(driver);
+        elementsPage.clickOnLogout(driver);
+        Assert.assertTrue(homePage.getHomePageHeaderText(driver).contains("Hey! Wait a minute... What is this site?"));
+
+    }
+
+    @Test(priority = 7)
+    public void testPerformanceButtonFunctionality()  {
+        driver.get(homePage.getHomePageUrl());
+        homePage.fillTheLoginForm(driver,userName,password);
+        elementsPage.fillSalesForm(driver, "Anton", "Angelov", "10000", 15000);
+        elementsPage.clickOnSubmit(driver);
+        Assert.assertTrue(elementsPage.isPerformanceButtonDisplayed(driver));
+        elementsPage.clickOnPerformanceButton(driver);
+        Assert.assertEquals("A positive result. Well done!", elementsPage.getPerformanceMessage(driver));
+        elementsPage.clickOnDeleteButton(driver);
+        elementsPage.clickOnLogout(driver);
+        Assert.assertTrue(homePage.getHomePageHeaderText(driver).contains("Hey! Wait a minute... What is this site?"));
+
+    }
+
+
+
+    @Test(priority = 8)
+    public void testSummaryTable()  {
+        driver.get(homePage.getHomePageUrl());
+        homePage.fillTheLoginForm(driver,userName,password);
         elementsPage.fillSalesForm(driver, "Plamen", "Petrov", "5000", 8000);
         elementsPage.clickOnSubmit(driver);
         elementsPage.fillSalesForm(driver, "Victor", "Markov", "75000", 4003);
@@ -166,7 +160,10 @@ public class TestPages {
     }
 
     @AfterTest
-    public void quitDriver() {
-        driver.quit();
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
+
 }
