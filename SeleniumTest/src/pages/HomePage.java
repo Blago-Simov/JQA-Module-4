990950c2d919c3f0634705ee02ada05910ef2890
Blagovest Simov <77917533+Blago-Simov@users.noreply.github.com>
2024-01-21 11:23:55 +0200

    Update HomePage.java

--- SeleniumTest/src/pages/HomePage.java
+++ SeleniumTest/src/pages/HomePage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
+
 
 
 public class HomePage {
@@ -23,24 +23,23 @@ public class HomePage {
 
     Waits waits = new Waits();
 
-    public String getHomePageUrl(WebDriver driver) {
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    public String getHomePageUrl(WebDriver driver){
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
         return homePageUrl;
     }
 
 
-    public String getHomePageHeaderText(WebDriver driver) {
-        waits.customWait(driver, Duration.ofSeconds(10), "presenceOfElement", homePageHeaderText);
+    public String getHomePageHeaderText(WebDriver driver){
+        waits.customWait(driver, Duration.ofSeconds(40), "presenceOfElement", homePageHeaderText);
         return driver.findElement(homePageHeaderText).getText();
     }
 
-    public void fillTheLoginForm(WebDriver driver, String arg1, String arg2) throws InterruptedException {
+    public void fillTheLoginForm(WebDriver driver, String arg1, String arg2){
         waits.customWait(driver, Duration.ofSeconds(20), "presenceOfElement", userNameTextField);
         driver.findElement(userNameTextField).sendKeys(arg1);
-        waits.customWait(driver, Duration.ofSeconds(10), "presenceOfElement", passwordTextField);
+        waits.customWait(driver, Duration.ofSeconds(20), "presenceOfElement", passwordTextField);
         driver.findElement(passwordTextField).sendKeys(arg2);
-        waits.customWait(driver, Duration.ofSeconds(10), "elementToBeClickable", loginButton);
+        waits.customWait(driver, Duration.ofSeconds(20), "elementToBeClickable", loginButton);
         driver.findElement(loginButton).click();
-        Thread.sleep(5000);
     }
 }
